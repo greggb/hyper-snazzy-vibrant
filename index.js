3c6bf50f39140e10572ba849dc61f300e1551636
Gregg Brewster <mail@digitalpuddle.com>
2016-12-29 12:34:13 -0800

    add vibrancy

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 'use strict';
 const foregroundColor = '#eff0eb';
-const backgroundColor = '#282a36';
+const backgroundColor = 'rgba(40, 42, 54, .6)';
+const black = '#282a36';
 const red = '#ff5c57';
 const green = '#5af78e';
 const yellow = '#f3f99d';
 const blue = '#57c7ff';
 const magenta = '#ff6ac1';
 const cyan = '#9aedfe';
-
+exports.onWindow = browserWindow => browserWindow.setVibrancy('ultra-dark');
 exports.decorateConfig = config => Object.assign({}, config, {
-	backgroundColor,
-	foregroundColor,
-	borderColor: '#222430',
-	cursorColor: '#97979b',
-	colors: {
-		black: backgroundColor,
-		red,
-		green,
-		yellow,
-		blue,
-		magenta,
-		cyan,
-		white: '#f1f1f0',
-		lightBlack: '#686868',
-		lightRed: red,
-		lightGreen: green,
-		lightYellow: yellow,
-		lightBlue: blue,
-		lightMagenta: magenta,
-		lightCyan: cyan,
-		lightWhite: foregroundColor
-	},
-	css: `
-		${config.css}
-		.tab_active:before {
-			border-color: rgba(255, 106, 193, 0.25);
-		}
-	`
+    backgroundColor,
+    foregroundColor,
+    borderColor: '#222430',
+    cursorColor: '#97979b',
+    colors: {
+        black,
+        red,
+        green,
+        yellow,
+        blue,
+        magenta,
+        cyan,
+        white: '#f1f1f0',
+        lightBlack: '#686868',
+        lightRed: red,
+        lightGreen: green,
+        lightYellow: yellow,
+        lightBlue: blue,
+        lightMagenta: magenta,
+        lightCyan: cyan,
+        lightWhite: foregroundColor
+    },
+    css: `
+        ${config.css}
+        .tab_active:before {
+            border-color: rgba(255, 106, 193, 0.25);
+        }
+    `
 });
